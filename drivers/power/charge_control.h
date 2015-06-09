cd6e1c9ce163f3a54b2e7fb0c8c82effa354037d
kirananto <kirananto@gmail.com>
2015-06-09 20:45:56 +0530

    initial

---
+++ drivers/power/charge_control.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright © 2015, KiranAnto "kirananto" <kirananto@gmail.com>
+ *
+ * Kernel header for yl_bq24157_charger Control driver
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+#ifndef __CHARGE_CONTROL_H__
+#define __CHARGE_CONTROL_H__
+extern int custom_current;
+extern int mswitch;
+#endif
